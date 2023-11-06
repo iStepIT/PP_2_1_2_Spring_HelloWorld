d1c6f875ed1fbdf338472e56583d564688974fc6
iliastepanov <stepanovpro@mail.ru>
2023-11-06 18:13:50 +0300

    done task

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,7 +6,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class AppTest {
